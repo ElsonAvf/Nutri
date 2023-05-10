52dc15a2d2e062fe436c7cf0e6b1890c28126ba9
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-05-10 16:31:17 -0300

    Remove unnecessary parenthesis

--- src/utils/fit_calc.js
+++ src/utils/fit_calc.js
@@ -4,7 +4,7 @@ const cmToMeter = heightInCm => heightInCm / 100
 class Fit {
   // Body Mass Index
   BMI(weight, height) {
-    return parseFloat(((weight / (height ** 2)) * 10000).toFixed(1))
+    return parseFloat((weight / (cmToMeter(height) ** 2)).toFixed(1))
   }
   // Lean Body Mass
   //Boer formula LMB
@@ -35,41 +35,37 @@ class Fit {
   // Harris Benedict BMR formula (1919)
   originalHarrisBenedictBMR(weight, height, age, gender) {
     if(gender === 'm') {
-      return Math.round(66.5 + (13.75 * weight) + (5.003 * height) - (6.755 * age))
+      return Math.round(66.5 + 13.7516 * weight + 5.0033 * height - 6.7550 * age)
     } else if(gender === 'f') {
-      return Math.round(655.1 + (9.56 * weight) + (1.8 * height) - (4.67 * age))
+      return Math.round(655.1 + 9.56 * weight + 1.85 * height - 4.676 * age)
     }
   }
   // Harris Benedict BMR formula revised by Rozan and Shizgan (1984)
   revisedHarrisBenedictBMR(weight, height, age, gender) {
     if(gender === 'm') {
-      return Math.round(88.362 + (13.397 * weight) + (4.799 * height) - (5.667 * age))
+      return Math.round(88.362 + 13.397 * weight + 4.799 * height - 5.667 * age)
     } else if(gender === 'f') {
-      return Math.round(447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age))
+      return Math.round(447.593 + 9.247 * weight + 3.098 * height - 4.330 * age)
     }
   }
   // Miffin-St Jeor BMR formula (1990)
-  miffinStJeorBMR(weight, height, age, gender) {
+  mifflinStJeorBMR(weight, height, age, gender) {
     if(gender === 'm') {
-      return Math.round((10 * weight) + (6.25 * height) - (5 * age) + 5)
+      return Math.round(10 * weight + 6.25 * height - 5 * age + 5)
     } else if(gender === 'f') {
-      return Math.round((10 * weight) + (6.25 * height) - (5 * age) - 161)
+      return Math.round(10 * weight + 6.25 * height - 5 * age - 161)
     }
   }
   // Katch-McArdle BMR formula (2001)
-  katchMcArdleBMR(weight, height, gender, LBM) {
-    if(typeof LBM === 'number') {
-      return Math.round(370 + (21.6 * LBM))
-    } else if(typeof LBM === 'function') {
-      return Math.round(370 + (21.6 * LBM(weight, height, gender)))
-    }
+  katchMcArdleBMR(LBM) {
+    return Math.round(370 + (21.6 * LBM))
   }
   // Schofield BMR formula (1985)
   schofieldBMR(weight, age, gender) {
     if(gender === 'm') {
       if(age > 17 && age < 30) {
         return Math.round(15.057 * weight + 692.2);
-      } else if(age > 29 && age < 59) {
+      } else if(age > 29 && age <= 59) {
         return Math.round(11.472 * weight + 873.1);
       } else if(age >= 60) {
         return Math.round(11.711 * weight + 587.7);
@@ -86,31 +82,31 @@ class Fit {
   };
   // Total Daily Energy Expenditure
   TDEE(BMR) {
-    return {
-      sedentary: Math.round(1.2 * BMR),
-      light: Math.round(1.3745 * BMR),
-      moderate: Math.round(1.55 * BMR),
-      heavy: Math.round(1.725 * BMR),
-      extremely: Math.round(1.9 * BMR)
-    }
+    return [
+      { activity: 'Sedentary', kcal: Math.round(1.2 * BMR) },
+      { activity: 'Light', kcal: Math.round(1.3745 * BMR) },
+      { activity: 'Moderate', kcal: Math.round(1.55 * BMR) },
+      { activity: 'Heavy', kcal: Math.round(1.725 * BMR) },
+      { activity: 'Extreme', kcal: Math.round(1.9 * BMR) }
+    ]
   }
   // Ideal Body Weight
   IBW(height, gender) {
-    return {
-      peterson: this.petersonIBW(height),
-      robinson: this.robinsonIBW(height, gender),
-      devine: this.devineIBW(height, gender),
-      miller: this.millerIBW(height, gender),
-      hamwi: this.hamwiIBW(height, gender),
-      broca: this.brocaIBW(height, gender),
-      lemmens: this.lemmensIBW(height),
-      bmi: this.BMIRangeIBW(height)
-    }
+    return [
+      { name: 'Peterson', result: this.petersonIBW(height) },
+      { name: 'Robinson', result: this.robinsonIBW(height, gender) },
+      { name: 'Devine', result: this.devineIBW(height, gender) },
+      { name: 'Miller', result: this.millerIBW(height, gender) },
+      { name: 'Hamwi', result: this.hamwiIBW(height, gender) },
+      { name: 'Broca', result: this.brocaIBW(height, gender) },
+      { name: 'Lemmens', result: this.lemmensIBW(height) },
+      { name: 'BMI', result: this.BMIRangeIBW(height) }
+    ]
   }
   // Peterson formula for IBW
   petersonIBW(height) {
     // Chosen BMI target 22 as it is uses in many calculators
-    return Math.round(2.2 * 22 + 3.5 * 22 * ((height / 100) - 1.5)) // Height in meters
+    return Math.round(2.2 * 22 + 3.5 * 22 * ((cmToMeter(height)) - 1.5))
   }
   // Devine formula for IBW
   devineIBW(height, gender) {
@@ -153,7 +149,7 @@ class Fit {
   }
   lemmensIBW(height) {
     // Ideal BMI = 22
-    return Math.round(22 * ((height / 100) ** 2)) // Height in meters
+    return Math.round(22 * ((cmToMeter(height)) ** 2))
   }
   BMIRangeIBW(height) {
     return {
