b9fb9690d2ca513b12e83a83fc1467aca550a907
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-08-23 17:51:21 -0300

    refactor: Remove unnecessary functions
    Those functions are now unncessary or are used in other parts of the
    project

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,63 +1,14 @@
-import uniqid from 'uniqid';
-
-const createStorage = () => {
+export const createStorage = () => {
   if(!localStorage.getItem('diary')) {
-    localStorage.setItem('diary', JSON.stringify([]))
+    localStorage.setItem('diary', JSON.stringify({
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+      snacks: [],
+    }))
   }
 }
 
-const getStorage = () => JSON.parse(localStorage.getItem('diary'));
+export const getStorage = () => JSON.parse(localStorage.getItem('diary'));
 
-const setStorage = newValue => localStorage.setItem('diary', JSON.stringify(newValue))
-
-const getMeals = id => {
-  const diary = getStorage()
-  for(let i = 0; i < diary.length; i++) {
-    if(diary[i].id === id) {
-      return diary[i]
-    }
-  }
-}
-
-const addMeals = title => {
-  const diary = getStorage()
-  diary.push({
-    id: uniqid(),
-    title, 
-    breakfast: [],
-    lunch: [],
-    dinner: [],
-    snacks: [],
-    kcalAmount: 0,
-    proteinAmount: 0,
-    carbAmount: 0,
-    fatAmount: 0,
-  })
-  setStorage(diary)
-}
-
-const deleteMeals = id => {
-  let diary = getStorage()
-  diary = diary.filter(meals => meals.id !== id)
-  setStorage(diary)
-}
-
-const updateMeals = newMeals => {
-  let diary = getStorage()
-  diary = diary.map(meals => {
-    if(meals.id === newMeals.id) {
-      return newMeals
-    }
-    return meals
-  })
-  setStorage(diary)
-}
-
-export {
-  createStorage,
-  getStorage,
-  getMeals,
-  addMeals,
-  deleteMeals,
-  updateMeals,
-}
+export const setStorage = newValue => localStorage.setItem('diary', JSON.stringify(newValue))
