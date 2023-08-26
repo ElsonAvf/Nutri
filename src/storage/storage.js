aee857e9fe010d0f6bb234801dd7944d3110ab40
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-08-26 14:58:05 -0300

    Move redux add & removeFood functions to storage
    As redux was removed these functions were moved to this file
    Also updated setStorage to trigger storage Event
    It is needed to watch localStorage changes

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,3 +1,5 @@
+import uniqid from 'uniqid';
+
 export const createStorage = () => {
   if(!localStorage.getItem('diary')) {
     localStorage.setItem('diary', JSON.stringify({
@@ -11,4 +13,23 @@ export const createStorage = () => {
 
 export const getStorage = () => JSON.parse(localStorage.getItem('diary'));
 
-export const setStorage = newValue => localStorage.setItem('diary', JSON.stringify(newValue))
+export const setStorage = newValue => {
+  localStorage.setItem('diary', JSON.stringify(newValue))
+  window.dispatchEvent(new Event('storage')) // As storage event needs to be trigged manually
+}
+
+export const addFood = (food, meal) => {
+  food.id = uniqid()
+  const meals = getStorage()
+  meals[meal].push(food)
+  setStorage(meals)
+};
+
+export const removeFood = (foodId, meal) => {
+  const meals = getStorage()
+  const updatedMeals = {
+    ...meals,
+    [meal]: meals[meal].filter(food => food.id !== foodId)
+  }
+  setStorage(updatedMeals)
+};
