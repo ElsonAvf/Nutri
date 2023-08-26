3b80c7c8c6981d103a0ecd47607ccc03aa079d08
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-08-26 10:54:49 -0300

    Create GenderFieldset component

--- src/pages/Calculator/components/GenderFieldset.jsx
+++ src/pages/Calculator/components/GenderFieldset.jsx
@@ -1,34 +1,28 @@
 import Icon from '@mdi/react';
 import { mdiGenderMale, mdiGenderFemale } from '@mdi/js';
-import { GenderFieldset, GenderLabel, InputRadio } from './../styles/Calculator.style.js';
+import GenderLabel from './GenderLabel';
 
 const GenderField = ({ gender, setGender }) => {
   return (
-    <GenderFieldset>
+    <fieldset className='w-full flex justify-center gap-3'>
       <legend>Gênero</legend>
-      <GenderLabel gender='male'>
-        <Icon path={mdiGenderMale} size={2} color={gender === 'm' ? 'honeydew' : 'deepskyblue'} title='Homem' />
-        <InputRadio
-          type='radio'
-          name='gender'
-          value='m'
-          required
-          checked={gender === 'm'}
-          onChange={setGender}
-        />
-      </GenderLabel>
-      <GenderLabel gender='female'>
-        <Icon path={mdiGenderFemale} size={2} color={gender === 'f' ? 'honeydew' : 'hotpink'} title='Mulher' />
-        <InputRadio
-          type='radio'
-          name='gender'
-          value='f'
-          required
-          checked={gender === 'f'}
-          onChange={setGender}
-        />
-      </GenderLabel>
-    </GenderFieldset>
+      <GenderLabel
+        gender={gender}
+        value='m'
+        color='deepskyblue'
+        icon={mdiGenderMale}
+        title='Homem'
+        setGender={setGender}
+      />
+      <GenderLabel
+        gender={gender}
+        value='f'
+        color='hotpink'
+        icon={mdiGenderFemale}
+        title='Mulher'
+        setGender={setGender}
+      />
+    </fieldset>
   );
 };
 
