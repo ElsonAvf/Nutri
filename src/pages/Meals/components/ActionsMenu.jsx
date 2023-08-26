feab55201fda61af7985621bce62bfb5ac24cf19
Elson Alves <108842851+ElsonAvf@users.noreply.github.com>
2023-08-26 15:01:19 -0300

    Replace redux removeFood function

--- src/pages/Meals/components/ActionsMenu.jsx
+++ src/pages/Meals/components/ActionsMenu.jsx
@@ -1,17 +1,13 @@
-import { useDispatch } from 'react-redux';
 import Icon from '@mdi/react';
 import { mdiTrashCanOutline } from '@mdi/js';
-import { removeFood } from 'features/meals/mealsSlice.js';
+import { removeFood } from 'storage/storage.js';
 
 export default function ActionsMenu({ id, meal }) {
-  const dispatch = useDispatch()
-
   return (
     <div className='absolute top-0 right-0 p-1 bg-black/60 h-9'>
-      <button onClick={(e) => {
+      <button onClick={e => {
         e.stopPropagation()
-        dispatch(removeFood({ id, meal })
-        )
+        removeFood(id, meal)
       }}>
         <Icon path={mdiTrashCanOutline} size={1} color='ghostwhite' />
       </button>
